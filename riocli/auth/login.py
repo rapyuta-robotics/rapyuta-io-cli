1b2f8adc680833261eaddb0a991fbfac5b61fec6
Ankit R Gadiya <ankit.gadiya@rapyuta-robotics.com>
2022-05-18 11:55:54 +0530

    feat(auth): adds support for non-interactive login

--- riocli/auth/login.py
+++ riocli/auth/login.py
@@ -23,16 +23,31 @@ from riocli.utils.context import get_root_context
     help_headers_color='yellow',
     help_options_color='green',
 )
-@click.option('--email', prompt='Email',
+@click.option('--email', type=str,
               help='Email of the Rapyuta.io account')
-@click.option('--password', prompt='Password', hide_input=True,
+@click.option('--password', type=str,
               help='Password for the Rapyuta.io account')
+@click.option('--project', type=str, default=None,
+              help='Context will be set to the Project after authentication')
+@click.option('--interactive/--no-interactive', is_flag=True, type=bool, default=True,
+              help='Make login interactive')
 @click.pass_context
-def login(ctx: click.Context, email: str, password: str):
+def login(ctx: click.Context, email: str, password: str, project: str, interactive: bool):
     """
     Log into the Rapyuta.io account using the CLI. This is required to use most of the
     functionalities of the CLI.
     """
+
+    if interactive:
+        email = email or click.prompt('Email')
+        password = password or click.prompt('Password', hide_input=True)
+
+    if not email:
+        click.secho('email not specified')
+        exit(1)
+    if not password:
+        click.secho('password not specified')
+        exit(1)
 
     ctx = get_root_context(ctx)
     ctx.obj.data['email_id'] = email
@@ -40,13 +55,16 @@ def login(ctx: click.Context, email: str, password: str):
     ctx.obj.data['auth_token'] = get_token(email, password)
 
     # Save if the file does not already exist
-    if not ctx.obj.exists:
-        click.echo('Logged in successfully!')
+    if not ctx.obj.exists or not interactive:
         ctx.obj.save()
     else:
         click.echo("[Warning] rio already has a config file present")
         click.confirm('Do you want to override the config', abort=True)
 
-    select_project(ctx.obj)
+    if not interactive and not project:
+        click.echo('Logged in successfully!')
+        return
+
+    select_project(ctx.obj, project=project)
     ctx.obj.save()
     click.echo('Logged in successfully!')
