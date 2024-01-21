b5b3daf64dbfe10911dc09eafdcd6770a24af0c7
Pallab Pain <pallabkumarpain@gmail.com>
2024-01-22 00:41:19 +0530

    fix(deployment): raises exception when package depends not found
    This commit fixes the issue where unhelpful error message is displayed
    when a package depends referenced in a deployment manifest is not
    found.
    
    Wrike Ticket: https://www.wrike.com/open.htm?id=1237582859

--- riocli/deployment/model.py
+++ riocli/deployment/model.py
@@ -54,11 +54,16 @@ class Deployment(Model):
         return obj if guid else False
 
     def create_object(self, client: Client, **kwargs) -> typing.Any:
-        pkg_guid, pkg = self.rc.find_depends(self.metadata.depends,
-                                             self.metadata.depends.version)
+        pkg_guid, pkg = self.rc.find_depends(
+            self.metadata.depends,
+            self.metadata.depends.version)
 
         if pkg_guid:
             pkg = client.get_package(pkg_guid)
+
+        if not pkg:
+            raise ValueError('package not found: {}'.format(self.metadata.depends))
+
         pkg.update()
 
         default_plan = pkg['plans'][0]
