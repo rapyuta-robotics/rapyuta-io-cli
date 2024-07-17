552267eaeb01cc77cc3ae6d74c4f11335536c344
Smruti Ranjan Senapati <smrtsenapati59@gmail.com>
2024-07-17 19:36:39 +0530

    fix(v2client): fix v2 client org header

--- riocli/v2client/error.py
+++ riocli/v2client/error.py
@@ -1,4 +1,3 @@
-
 class RetriesExhausted(Exception):
     def __init__(self, msg=None):
         Exception.__init__(self, msg)
@@ -8,6 +7,19 @@ class DeploymentNotRunning(Exception):
     def __init__(self, msg=None):
         Exception.__init__(self, msg)
 
+
 class ImagePullError(Exception):
     def __init__(self, msg=None):
         Exception.__init__(self, msg)
+
+
+class NetworkNotFound(Exception):
+    def __init__(self, message='network not found!'):
+        self.message = message
+        super().__init__(self.message)
+
+
+class DeploymentNotFound(Exception):
+    def __init__(self, message='deployment not found!'):
+        self.message = message
+        super().__init__(self.message)
