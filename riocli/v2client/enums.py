0536e338215f72c5eb6afcf7f11b42e86980cfbd
Smruti Ranjan Senapati <smrtsenapati59@gmail.com>
2024-07-22 02:32:18 +0530

    feat(v2client): adds poll methods and stream deployment logs method

--- riocli/v2client/enums.py
+++ riocli/v2client/enums.py
@@ -29,3 +29,19 @@ class DeploymentStatusConstants(str, enum.Enum):
     DeploymentStatusError = "Error"
     DeploymentStatusUnknown = "Unknown"
     DeploymentStatusStopped = "Stopped"
+
+
+class DiskStatusConstants(str, enum.Enum):
+    """
+    Enumeration variables for the deployment status
+
+    """
+
+    def __str__(self):
+        return str(self.value)
+
+    DiskStatusAvailable = "Available"
+    DiskStatusBound = "Bound"
+    DiskStatusReleased = "Released"
+    DiskStatusFailed = "Failed"
+    DiskStatusPending = "Pending"
