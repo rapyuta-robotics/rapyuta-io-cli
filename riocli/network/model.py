5924f539426d3647290ec1fc99ed674575ec85ed
Smruti Ranjan Senapati <smrtsenapati59@gmail.com>
2024-07-22 02:29:23 +0530

    fix(network): fix network commands

--- riocli/network/model.py
+++ riocli/network/model.py
@@ -12,23 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import typing
-from typing import Union, Any, Dict
-from munch import munchify, unmunchify
+from typing import Any, Dict
+
 import click
-import time
+from munch import unmunchify
 
-from rapyuta_io import Client
-from rapyuta_io.clients.common_models import Limits
-from rapyuta_io.clients.native_network import NativeNetwork, \
-    Parameters as NativeNetworkParameters
-from rapyuta_io.clients.routed_network import RoutedNetwork, \
-    Parameters as RoutedNetworkParameters
-
+from riocli.config import new_v2_client
+from riocli.constants import Colors, Symbols
 from riocli.jsonschema.validate import load_schema
 from riocli.model import Model
-from riocli.v2client.client import NetworkNotFound
-from riocli.config import new_v2_client
-from riocli.constants import Colors, Symbols, Status
+from riocli.v2client.client import NetworkNotFound, Client
+
 
 class Network(Model):
     def __init__(self, *args, **kwargs):
@@ -37,7 +31,7 @@ class Network(Model):
     def find_object(self, client: Client) -> bool:
         try:
             network, obj = self.rc.find_depends({"kind": self.kind.lower(),
-                            "nameOrGUID": self.metadata.name}, self.spec.type)
+                                                 "nameOrGUID": self.metadata.name}, self.spec.type)
             if not network:
                 return False
 
@@ -45,45 +39,26 @@ class Network(Model):
         except NetworkNotFound:
             return False
 
-    def poll_deployment_till_ready(self, client: Client, network: typing.Any, retry_count = 50, sleep_interval = 6):
-        for _ in range(retry_count):
-            if network.status and network.status.status == Status.RUNNING:
-                return network
-
-            time.sleep(sleep_interval)
-            network = client.get_network(network.metadata.name)
-        return network
-
     def create_object(self, client: Client, **kwargs) -> typing.Any:
         client = new_v2_client()
 
-        # convert to a dict and remove the ResolverCache
-        # field since it's not JSON serializable
-        network = unmunchify(self)
-        network.pop("rc", None)
-        r = client.create_network(network)
+        r = client.create_network(self._sanitize_network())
 
         retry_count = int(kwargs.get('retry_count'))
         retry_interval = int(kwargs.get('retry_interval'))
 
         try:
-            r = self.poll_deployment_till_ready(
-                client = client,
-                network = r,
-                retry_count=retry_count,
-                sleep_interval=retry_interval,
-            )
+            r = client.poll_network(r.metadata.name, retry_count=retry_count, sleep_interval=retry_interval)
         except Exception as e:
             click.secho(">> {}: Error polling for network ({}:{})".format(
                 Symbols.WARNING,
                 self.kind.lower(),
                 self.metadata.name), fg=Colors.YELLOW)
-
+            click.secho(str(e), fg=Colors.YELLOW)
 
         return unmunchify(r)
 
-    def update_object(self, client: Client,
-                      obj: Union[RoutedNetwork, NativeNetwork]) -> Any:
+    def update_object(self, client: Client, obj: typing.Any) -> Any:
         pass
 
     def delete_object(self, client: Client, obj: typing.Any) -> typing.Any:
@@ -94,10 +69,6 @@ class Network(Model):
     def pre_process(cls, client: Client, d: Dict) -> None:
         pass
 
-    def _get_limits(self):
-        return Limits(self.spec.resourceLimits['cpu'],
-                      self.spec.resourceLimits['memory'])
-
     @staticmethod
     def validate(data):
         """
@@ -105,3 +76,16 @@ class Network(Model):
         """
         schema = load_schema('network')
         schema.validate(data)
+
+    def _sanitize_network(self) -> typing.Dict:
+        # Unset createdAt and updatedAt to avoid timestamp parsing issue.
+        self.metadata.createdAt = None
+        self.metadata.updatedAt = None
+
+        data = unmunchify(self)
+
+        # convert to a dict and remove the ResolverCache
+        # field since it's not JSON serializable
+        data.pop("rc", None)
+
+        return data
--- riocli/network/util.py
+++ riocli/network/util.py
@@ -12,89 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import functools
-from typing import Optional, Union, Tuple, Callable, Any, List
+from typing import List
 from munch import Munch
-import click
 import re
 
-from rapyuta_io import DeploymentPhaseConstants, Client
-from rapyuta_io.clients.native_network import NativeNetwork
-from rapyuta_io.clients.routed_network import RoutedNetwork
-
-from riocli.config import new_client
-from riocli.constants import Colors
 from riocli.utils import tabulate_data
-from riocli.utils.selector import show_selection
-from riocli.config import new_v2_client
 from riocli.network.model import Network
-
-def name_to_guid(f: Callable) -> Callable:
-    @functools.wraps(f)
-    def decorated(**kwargs: Any):
-        client = new_v2_client()
-
-        name = kwargs.pop('network_name')
-        network_type = kwargs.pop('network', None)
-        guid = None
-        if name.startswith('net-'):
-            guid = name
-            name = None
-
-        try:
-            if guid:
-                network = find_network_guid(
-                    client, guid, network_type)
-                name = network.metadata.name
-
-            else:
-                network = find_network_name(
-                    client, name, network_type)
-        except Exception as e:
-            click.secho(str(e), fg=Colors.RED)
-            raise SystemExit(1) from e
-
-        kwargs['network_type'] = network_type
-        kwargs['network_name'] = name
-        kwargs['network_guid'] = guid
-        f(**kwargs)
-
-    return decorated
-
-
-def find_network_guid(
-        client: Client,
-        guid: str,
-        network_type: str,
-) -> Munch:
-
-    if network_type:
-        networks = client.list_networks(query={'network_type': network_type})
-    else:
-        networks = client.list_networks()
-
-    for network in networks:
-        if network.status and network.status.phase == DeploymentPhaseConstants.DEPLOYMENT_STOPPED.value:
-            continue
-        if guid == network.metadata.guid:
-            return network
-
-    raise NetworkNotFound()
-
-def find_network_guid(client: Client, name: str, version: str = None) -> str:
-    packages = client.get_all_packages(name=name, version=version)
-    if len(packages) == 0:
-        click.secho("package not found", fg='red')
-        raise SystemExit(1)
-
-    if len(packages) == 1:
-        return packages[0].packageId
-
-    options = {}
-    for pkg in packages:
-        options[pkg.packageId] = '{} ({})'.format(pkg.packageName, pkg.packageVersion)
-
-    choice = show_selection(options, header='Following packages were found with the same name')
-    return choice
+from riocli.v2client import Client
 
 
 def fetch_networks(
@@ -103,14 +27,13 @@ def fetch_networks(
         network_type: str,
         include_all: bool,
 ) -> List[Network]:
-
     if network_type:
         networks = client.list_networks(query={'network_type': network_type})
     else:
         networks = client.list_networks()
 
     if include_all:
-        return networks
+        return list(networks)
 
     result = []
     for n in networks:
@@ -118,6 +41,7 @@ def fetch_networks(
             result.append(n)
 
     return result
+
 
 def print_networks_for_confirmation(networks: List[Munch]) -> None:
     headers = ['Name', 'Type']
