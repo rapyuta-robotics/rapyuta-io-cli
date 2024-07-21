6672b748208144b7579a0c030d20dcdda11ed6d9
Smruti Ranjan Senapati <smrtsenapati59@gmail.com>
2024-07-22 02:30:08 +0530

    fix(disk): fix disk commands

--- riocli/disk/util.py
+++ riocli/disk/util.py
@@ -14,18 +14,9 @@
 import typing
 import re
 
-from rapyuta_io import Client
-from rapyuta_io.clients.persistent_volumes import DiskCapacity, DiskType
-from rapyuta_io.utils.rest_client import RestClient, HttpMethod
-
-from riocli.config import Configuration, new_client
-from riocli.constants import Colors, Symbols
 from riocli.disk.model import Disk
 from riocli.utils import tabulate_data
-
-class DiskNotFound(Exception):
-    def __init__(self):
-        super().__init__('Disk not found')
+from riocli.v2client import Client
 
 
 def fetch_disks(
@@ -33,7 +24,6 @@ def fetch_disks(
         disk_name_or_regex: str,
         include_all: bool,
 ) -> typing.List[Disk]:
-
     disks = client.list_disks()
 
     if include_all:
@@ -46,25 +36,25 @@ def fetch_disks(
 
     return result
 
-def is_disk_ready(client: Client , name: str) -> bool:
-    disk = client.get_disk(name)
-    return disk.status.get("status", "") == "Available"
 
 def display_disk_list(disks: typing.Any, show_header: bool = True):
     headers = []
     if show_header:
         headers = (
-            'Disk ID', 'Name', 'Status', 'Capacity',
-            'Used', 'Available', 'Used By',
+            'Disk ID', 'Name', 'Status', 'Capacity (GB)',
+            'Capacity Used (GB)', 'Used By',
         )
 
-    data = [[d.metadata.guid,
-             d.metadata.name,
-             d.status.get("status"),
-             d.spec.capacity,
-             d.spec.get("CapacityUsed"),
-             d.spec.get("CapacityAvailable"),
-             d.get("diskBound", {}).get("DeploymentName")]
-            for d in disks]
+    data = []
+
+    for d in disks:
+        capacity = d.status.get("capacityUsed", 0) / (1024 * 1204 * 1024)  # Bytes -> GB
+
+        data.append([d.metadata.guid,
+                     d.metadata.name,
+                     d.status.get("status"),
+                     d.spec.capacity,
+                     capacity,
+                     d.status.get("diskBound", {}).get("deployment_name")])
 
     tabulate_data(data, headers)
