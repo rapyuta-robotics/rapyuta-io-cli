3c739774cddf1d8706d23c194528d5627a3de5ea
Pallab Pain <pallabkumarpain@gmail.com>
2024-08-02 19:55:17 +0530

    fix(deployment): replace aggregateStatus with status
    The aggregateStatus field is no longer sent in the API response.
    This commit removes all usages of aggregateStatus and replaces it
    with status.

--- riocli/deployment/status.py
+++ riocli/deployment/status.py
@@ -32,7 +32,7 @@ def status(deployment_name: str) -> None:
     try:
         client = new_v2_client()
         deployment = client.get_deployment(deployment_name)
-        click.secho(deployment.status.aggregateStatus)
+        click.secho(deployment.status.status)
     except Exception as e:
         click.secho(str(e), fg=Colors.RED)
         raise SystemExit(1)
