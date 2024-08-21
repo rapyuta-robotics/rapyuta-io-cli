f548d8c8b6c9b36e64815ed8d3c075deefe57c7f
Pallab Pain <pallabkumarpain@gmail.com>
2024-08-22 00:30:53 +0530

    feat(deployment): wait until all dependecies are running
    This commit adds a field in the deployment schema to wait for
    dependencies to come to a running state before a deployment is created.

--- riocli/deployment/model.py
+++ riocli/deployment/model.py
@@ -11,14 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import time
 import typing
 
 from munch import unmunchify
 
 from riocli.config import new_v2_client
+from riocli.constants import Status
 from riocli.exceptions import ResourceNotFound
 from riocli.model import Model
-from riocli.v2client.error import HttpAlreadyExistsError, HttpNotFoundError
+from riocli.v2client import Client
+from riocli.v2client.error import HttpAlreadyExistsError, HttpNotFoundError, RetriesExhausted
 
 
 class Deployment(Model):
@@ -27,7 +30,13 @@ class Deployment(Model):
         self.update(*args, **kwargs)
 
     def apply(self, *args, **kwargs) -> typing.Any:
+        hard_dependencies = [d.nameOrGUID for d in self.spec.get('depends', []) if d.get('wait', False)]
+
         client = new_v2_client()
+
+        # Block until all hard dependencies are in RUNNING state
+        if hard_dependencies:
+            wait_for_dependencies(client, hard_dependencies)
 
         self.metadata.createdAt = None
         self.metadata.updatedAt = None
@@ -44,3 +53,24 @@ class Deployment(Model):
             client.delete_deployment(self.metadata.name)
         except HttpNotFoundError:
             raise ResourceNotFound
+
+
+def wait_for_dependencies(
+        client: Client,
+        deployment_names: typing.List[str],
+        retry_count: int = 50,
+        retry_interval: int = 6,
+) -> None:
+    """Waits until all deployment_names are in RUNNING state."""
+    for _ in range(retry_count):
+        deployments = client.list_deployments(query={
+            'names': deployment_names,
+            'phases': ['InProgress', 'Provisioning', 'Succeeded']
+        })
+
+        if all(d.status.status == Status.RUNNING for d in deployments):
+            return
+
+        time.sleep(retry_interval)
+
+    raise RetriesExhausted(f'Retries exhausted waiting for dependencies: {deployment_names}')
