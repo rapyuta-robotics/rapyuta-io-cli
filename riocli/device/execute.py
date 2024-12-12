fd6fb253e8e47c27533a02c4ab86abb869f47eb5
Pallab Pain <pallabkumarpain@gmail.com>
2024-12-12 12:47:18 +0530

    feat(device): execute command on multiple devices

--- riocli/device/execute.py
+++ riocli/device/execute.py
@@ -12,13 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import typing
-
 import click
 from click_help_colors import HelpColorsCommand
-
+from riocli.config import new_client
 from riocli.constants import Colors
-from riocli.device.util import name_to_guid
-from riocli.utils.execute import run_on_device
+from riocli.device.util import fetch_devices
+from rapyuta_io import Command
+from shlex import join
 
 
 @click.command(
@@ -37,19 +37,17 @@ from riocli.utils.execute import run_on_device
     default=False,
     help="Run the command asynchronously.",
 )
-@click.argument("device-name", type=str)
+@click.argument("device-name-or-regex", type=str)
 @click.argument("command", nargs=-1)
-@name_to_guid
 def execute_command(
-    device_name: str,
-    device_guid: str,
+    device_name_or_regex: str,
     user: str,
     timeout: int,
     shell: str,
     run_async: bool,
     command: typing.List[str],
 ) -> None:
-    """Execute commands on a device.
+    """Execute commands on one or more devices.
 
     You can specify the user, shell, run-async, and timeout options to customize
     the command execution. To specify the user, use the --user flag.
@@ -63,19 +61,44 @@ def execute_command(
 
     Usage Examples:
 
-        $ rio device execute DEVICE_NAME "ls -l"
-    """
-    try:
-        response = run_on_device(
-            device_guid=device_guid,
-            user=user,
-            shell=shell,
-            command=command,
-            background=run_async,
-            timeout=timeout,
-        )
+        $ rio device execute DEVICE_NAME_OR_REGEX "ls -l"
 
-        click.secho(response)
+        To execute the command asynchronously:
+
+            $ rio device execute DEVICE_NAME_OR_REGEX "ls -l" --async
+
+        To run the command on all devices:
+
+            $ rio device execute ".*" "ls -l"
+    """
+
+    client = new_client()
+
+    try:
+        devices = fetch_devices(
+            client, device_name_or_regex, include_all=False, online_devices=True
+        )
     except Exception as e:
         click.secho(str(e), fg=Colors.RED)
         raise SystemExit(1) from e
+
+    if not devices:
+        click.secho("No device(s) found", fg=Colors.RED)
+        raise SystemExit(1)
+
+    device_guids = [d.uuid for d in devices if d.status == "ONLINE"]
+    device_dict = {d.uuid: d.name for d in devices}
+    cmd = join(tuple(["bash", "-c"]) + command)
+    try:
+        result = client.execute_command(
+            device_ids=device_guids,
+            command=Command(cmd, shell=shell, bg=run_async, runas=user, timeout=timeout),
+            timeout=timeout,
+        )
+        for device_guid in result:
+            click.secho(">>> {}({})".format(device_dict[device_guid], device_guid), fg=Colors.YELLOW)
+            click.echo("{}\n".format(result[device_guid]))
+    except Exception as e:
+        click.secho(str(e), fg=Colors.RED)
+        raise SystemExit(1) from e
+
