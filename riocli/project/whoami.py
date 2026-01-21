73283504924032f706ed33f5bec3a7653cb930dd
Ankit R Gadiya <ankit.gadiya@rapyuta-robotics.com>
2026-01-21 19:49:44 +0530

    fix(project): use myself API for finding role in project

--- riocli/project/whoami.py
+++ riocli/project/whoami.py
@@ -14,7 +14,7 @@
 import click
 from click_help_colors import HelpColorsCommand
 
-from riocli.config import Configuration
+from riocli.config import Configuration, get_config_from_context
 from riocli.constants import Colors
 from riocli.exceptions import LoggedOut
 from riocli.project.util import name_to_guid
@@ -37,11 +37,13 @@ def whoami(ctx: click.Context, project_name: str, project_guid: str) -> None:
     If you do not specify the project name, the command will use the project
     set in the CLI context.
     """
-    if not ctx.obj.data.get("email_id"):
+    config = get_config_from_context(ctx)
+
+    if not config.data.get("email_id"):
         raise LoggedOut
 
     try:
-        role = find_role(ctx.obj, project_guid)
+        role = find_role(config, project_guid)
         click.echo(role)
     except SystemExit as e:
         click.secho(str(e), fg=Colors.RED)
@@ -50,8 +52,8 @@ def whoami(ctx: click.Context, project_name: str, project_guid: str) -> None:
 
 def find_role(
     config: Configuration,
-    project_guid: str = None,
-) -> str:
+    project_guid: str | None = None,
+) -> list[str]:
     """
     Find the role of the user in the project.
 
@@ -63,35 +65,14 @@ def find_role(
     """
     v2_client = config.new_v2_client()
 
-    # The user email comes from the config
-    user_email: str = config.data.get("email_id")
-    if not user_email:
-        raise ValueError("User email cannot be found in the config.")
-
     # Default to the config values if not provided
-    if not project_guid:
+    if project_guid is None:
         project_guid = config.data.get("project_id")
 
-    if not project_guid:
-        raise ValueError("Project GUID cannot be found.")
+    user = v2_client.get_myself()
 
-    try:
-        project = v2_client.get_project(project_guid=project_guid)
-    except Exception as e:
-        raise e
+    for project in user.spec.projects:
+        if project.guid == project_guid:
+            return project.role_names
 
-    roles = []
-    for member in getattr(project.spec, "members", []):
-        if (
-            member.subject.kind == "User"
-            and member.subject.name.casefold() == user_email.casefold()
-        ):
-            role_names = getattr(member, "role_names", []) or []
-            implicit_role_names = getattr(member, "implicit_role_names", []) or []
-            roles.extend(role_names)
-            roles.extend(implicit_role_names)
-
-    if not roles:
-        raise Exception("User does not have access to the project")
-
-    return roles
+    raise Exception("User does not have access to the project")
