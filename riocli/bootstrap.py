bddb51893a364d46061bbb5515f031a0804278ea
Ankit Gadiya <ankit.gadiya@rapyuta-robotics.com>
2026-06-02 15:25:06 +0530

    fix(ssh): rename the command to `ssh-cert`

--- riocli/bootstrap.py
+++ riocli/bootstrap.py
@@ -67,6 +67,7 @@ from riocli.vpn import vpn
         "sr": "static-route",
         "ug": "usergroup",
         "sa": "service-account",
+        "ssh": "ssh-cert",
     },
     help_headers_color=Colors.YELLOW,
     help_options_color=Colors.GREEN,
